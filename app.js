0c461a7738fee38f11f2ffea5244f54348778523
Lenon <64690648+LenonTG@users.noreply.github.com>
2020-08-10 21:27:26 +0200

    Solution

--- app.js
+++ app.js
@@ -1,60 +1,33 @@
-(function () {
-'use strict';
+(function() {
+    'use strict';
 
-  angular.module('LunchCheck', [])
-    .controller('LunchCheckController', LunchCheckController);
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
 
-  LunchCheckController.$inject = ['$scope'];
-  function LunchCheckController($scope) {
-    $scope.textbox = "";
-    $scope.message = "";
-    $scope.fontStyle = {};
-    $scope.boxStyle = {};
-        
-    $scope.counteWords = function () {
-      if ($scope.textbox !== false) {
-        var arr = $scope.textbox.split(',');
-        var count = 0;
-        for(var i = 0; i< arr.length; i++){
-          if (arr[i].trim() !=='')
-            count++;
-        }
-            if (count > 0) {
-              if (count <= 3) {
-                $scope.message = 'Enjoy!';
-              }
-              else if (count > 3) {
-                $scope.message = 'Too much!'
-              }
-              $scope.fullBox();
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController($scope) {
+        $scope.dishes = '';
+        $scope.message = '';
+        $scope.checked = false;
+
+        $scope.checkLunch = function() {
+            if ($scope.dishes.trim().length === 0) {
+                $scope.empty = true;
+            } else {
+                $scope.checked = true;
+                $scope.empty = false;
+                var arrayDishes = $scope.dishes.split(',');
+                var arrayDishesWithoutEmptys = arrayDishes.filter(function(v) {
+                    return v.trim() !== '';
+                });
+
+                if (arrayDishesWithoutEmptys.length <= 3) {
+                    $scope.message = 'Enjoy!';
+                } else {
+                    $scope.message = 'Too much!';
+                }
             }
-            else {
-              $scope.emptyBox();
-            }
-        }
-        else {
-          $scope.emptyBox();
-        }
-      }
-      $scope.emptyBox = function() {
-        $scope.message = 'Please enter data first';
-          $scope.fontStyle = {
-            "color":"red"
-          };
-          $scope.boxStyle = {
-            "border-color":"red"
-          };
-      };
-
-      $scope.fullBox = function() {
-          $scope.fontStyle = {
-            "color":"green"
-          };
-          $scope.boxStyle = {
-            "border-color":"green"
-          };
-      };
-
-  } 13 $scope.counteWords = function () 
-
+        };
+    }
 })();
